589cd972f8575edf660d8c22e96c0eb49046c5e0
Class01 <Class01@DESKTOP-46EHI55>
2023-04-27 14:04:33 +0900

    jungol

--- Jungol1/src/q515/Main.java
+++ Jungol1/src/q515/Main.java
@@ -1,15 +1,15 @@
-package q9015;
+package q515;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		System.out.print("두 수를 입력하시오. " );
+		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
-		System.out.printf("%d + %d = %d\n",num1,num2,num1+num2);
 		System.out.printf("%d * %d = %d\n",num1,num2,num1*num2);
+		System.out.printf("%d / %d = %d\n",num1,num2,num1/num2);
+
 	}
 
 }
