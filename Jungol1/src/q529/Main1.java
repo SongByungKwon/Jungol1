672f1d62b926f6a5f5a7ebdb13a88d1c2395ef06
Class01 <Class01@DESKTOP-46EHI55>
2023-04-28 18:15:33 +0900

    jungol

--- Jungol1/src/q529/Main1.java
+++ Jungol1/src/q529/Main1.java
@@ -2,17 +2,18 @@ package q529;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main1 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int i = sc.nextInt(); //키
 		int n = sc.nextInt(); //몸무게
 		sc.close();
+		int all= n+100-i;// 비만도
 		
-		System.out.printf("%d\n",n+100-i); //몸무게+100-키
+		System.out.printf("%d\n",all); //몸무게+100-키
 		
-		if(n+100-i>0) {      //몸무게+100-키>0
+		if(all>0) {      //몸무게+100-키>0
 			System.out.printf("Obesity");
 		}
 		else {
---
+++ Jungol1/src/q536/Main.java
@@ -0,0 +1,50 @@
+//package q536;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		int i=0;
+//		
+//		while(i<15) {
+//			i++;
+//			System.out.print(i+" "); //(i++ +" ")
+//		}
+//
+//	}
+//
+//}
+package q536;
+
+public class Main {
+
+	public static void main(String[] args) {
+	for(int i=0;i<=15;i++) {
+//		System.out.print(i+"?");
+		System.out.print(i);
+		if(i <15) {
+		System.out.print("?");
+		}
+		
+	}
+
+	}
+
+}
+
+//package q536;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//	int i=0;
+//	do {
+//		i= i+1;
+//	System.out.print(i+" ");
+//	}while(i<15);
+//	
+//			
+//
+//	}
+//
+//}
+
