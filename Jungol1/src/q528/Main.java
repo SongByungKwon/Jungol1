361feae2b3a75f1488f8c11ddc57cd0e47a573a9
Class01 <Class01@DESKTOP-46EHI55>
2023-04-28 13:44:28 +0900

    jungol

--- Jungol1/src/q528/Main.java
+++ Jungol1/src/q528/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
+		sc.close();
 		System.out.println(num1);
 		if(num1<0) {
 			System.out.println("minus");
