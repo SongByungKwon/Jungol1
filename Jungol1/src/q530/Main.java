e861e927e760e7373ec1d172383d45cab671f11c
Class01 <Class01@DESKTOP-46EHI55>
2023-05-01 10:06:12 +0900

    jungol

---
+++ Jungol1/src/q530/Main.java
@@ -0,0 +1,19 @@
+package q530;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int old = sc.nextInt();
+		sc.close();
+		if(old>=20) {
+			System.out.println("adult");
+		}
+		else {
+			System.out.printf("%d years later",20-old);
+			
+		}
+	}
+}
