ad3877c4dbc5beb9ff3717202564365246fb7666
Class01 <Class01@DESKTOP-46EHI55>
2023-05-10 17:59:56 +0900

    jungol

--- Jungol1/src/q522/Main.java
+++ Jungol1/src/q522/Main.java
@@ -1,14 +1,14 @@
-package q522;
-
-import java.util.Scanner;
-
-public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
-		int num1= sc.nextInt();
-		int num2= sc.nextInt();
-		sc.close();
+//package q522;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//	public static void main(String[] args) {
+//		Scanner sc =new Scanner(System.in);
+//		int num1= sc.nextInt();
+//		int num2= sc.nextInt();
+//		sc.close();
 //		boolean cut=true;
 //		if(num1==num2) {
 //			System.out.println(cut);
@@ -22,6 +22,7 @@ public class Main {
 //			System.out.println(cut);
 //		}
 //		
+//		------------------------------------
 //		if(num1== num2) {
 //			System.out.println("true");
 //		}
@@ -35,7 +36,7 @@ public class Main {
 //			System.out.println("false");
 //		}
 		
-		
+//		----------------------------------
 		
 //		if(num1==num2) {
 //			System.out.println(1);
@@ -45,12 +46,13 @@ public class Main {
 //			System.out.println(0);
 //			System.out.println(1);
 //		}
-		boolean a = (num1==num2) ? true:false;
-		System.out.println(a);
-		a = (num1!=num2) ? true:false;
-		System.out.println(a);
-		
-	}
-
-}
+//		-------------------------------------
+//		boolean a = (num1==num2) ? true:false;
+//		System.out.println(a);
+//		a = (num1!=num2) ? true:false;
+//		System.out.println(a);
+//		
+//	}
+//
+//}
 
