8e00f10cfcb1054639456c80ee55f6e158a0676b
Class01 <Class01@DESKTOP-46EHI55>
2023-06-02 13:51:07 +0900

    jungol

--- Jungol1/src/q600/Main.java
+++ Jungol1/src/q600/Main.java
@@ -1,25 +1,37 @@
 package q600;
 
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc= new Scanner(System.in);
-		String str= sc.nextLine();
-		char c[]=new char[100];
-		int cnt=1;
+		String str = sc.nextLine();
+//		char c[]=new char[100];
+//		int cnt=1;
 		sc.close();
 		
-		for(int i=0; i<str.length();i++) {
-			c[i]= str.charAt(i);
-			if(c[i]==' ') {
-				cnt++;
-			}
-			
-		}
+//		for(int i=0; i<str.length();i++) {
+//			c[i]= str.charAt(i);
+//			if(c[i]==' ') {
+//				cnt++;
+//			}
+//			
+//		}
+//		
+//		System.out.print(cnt);
 		
-		System.out.print(cnt);
+//---------------------------------------------
+	//2nd:StringTokenizer
+		StringTokenizer st =new StringTokenizer(str);
+		int n  = st.countTokens();
+		System.out.println(n);
+		
+		while(st.hasMoreTokens()) {
+			String token = st.nextToken();
+			System.out.println(token);
+		}
 	}
 
 }
